c33afa80476dc277def832684dc13a3b02e6aa23
Kathleen Dollard <kathleen.a.dollard@gmail.com>
2018-12-05 07:05:59 -0800

    Copy code

---
+++ CSharp7Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+namespace CSharp7Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+//            IDictionary<int, Person> persons = GetData();
+//            var term = new Spring2016TermMessaging();
+//            var staffCount = 0;
+//            IEnumerable<string> thankYouMessages = term.GetThankYouMessages(
+//                    persons.Values, out staffCount);
+
+//            Console.WriteLine($"Staff count: {staffCount}");
+//            Console.Write(string.Join("\n", thankYouMessages));
+//            Console.Read();
+//        }
+
+//        private static IDictionary<Int32, Person> GetData()
+//        {
+//            ISeedData<Person> seedData = new SeedDataForPerson();
+//            return seedData.GetData();
+//        }
+
+//        private static Person GetPerson(
+//                        IDictionary<int, Person> persons, int id)
+//        {
+//            return persons[id];
+//        }
+//    }
+//}
---
+++ Domain/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp7Demo
+{
+    public class Person
+    {
+        public Person(string name) => Name = name;
+        public string Name { get; }
+    }
+
+    public class Student : Person
+    {
+        public Student(string name, decimal gpa) : base(name) => GPA = gpa;
+        public decimal GPA { get; }
+    }
+
+    public class Staff : Person
+    {
+        public Staff(string name, StaffRole staffRole, decimal salary) : base(name)
+        {
+            StaffRole = staffRole;
+            Salary = salary;
+        }
+        public StaffRole StaffRole { get; }
+        public decimal Salary { get; }
+    }
+
+    public class Instructor : Staff
+    {
+        public Instructor(string name, decimal salary, IEnumerable<string> courses)
+            : base(name, StaffRole.Instructor, salary) => Courses = courses;
+
+        public IEnumerable<string> Courses { get; private set; }
+    }
+}
